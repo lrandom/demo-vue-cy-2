e06cd561ec0fa4888b56712f3dc3cf77943ba7e5
Nguyễn Thành Luân <luannguyen@prep.vn>
2024-11-04 09:27:41 +0700

    async component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Parent from '@/components/Parent.vue'
 import ParentComponent from '@/components/ParentComponent.vue'
+import AsyncComponentParent from '@/components/AsyncComponentParent.vue'
 </script>
 
 <template>
-  <ParentComponent :component-type="1" />
+<!--  <ParentComponent :component-type="1" />
   <ParentComponent :component-type="2" />
-  <ParentComponent :component-type="3" />
+  <ParentComponent :component-type="3" />-->
+  <AsyncComponentParent/>
 </template>
---
+++ src/components/AsyncComponentParent.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { computed, defineAsyncComponent, ref } from 'vue'
+
+const loadComponent = ref(false)
+
+const component = computed(() => {
+  return loadComponent.value
+    ? defineAsyncComponent({
+        loader: () => import('./sub/A.vue'),
+      })
+    : null
+})
+</script>
+
+<template>
+  <component :is="component" />
+
+  <button @click="loadComponent = true">Click Me hihi</button>
+</template>
+
+<style scoped></style>
