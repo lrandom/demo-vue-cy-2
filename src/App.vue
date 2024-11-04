b5e3e40b079e4c230771ccb8de22a8846b690d15
Nguyễn Thành Luân <luannguyen@prep.vn>
2024-11-04 09:18:53 +0700

    add built in component demo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
 import Parent from '@/components/Parent.vue'
+import ParentComponent from '@/components/ParentComponent.vue'
 </script>
 
 <template>
-  <Parent></Parent>
+  <ParentComponent :component-type="1" />
+  <ParentComponent :component-type="2" />
+  <ParentComponent :component-type="3" />
 </template>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,38 @@
+<script setup>
+import A from '@/components/sub/A.vue'
+import B from '@/components/sub/B.vue'
+import C from '@/components/sub/C.vue'
+import { computed } from 'vue'
+
+const props = defineProps({
+  componentType: {
+    type: Number,
+    required: true,
+    default: 1,
+  },
+})
+
+const component = computed(() => {
+  switch (props.componentType) {
+    case 1:
+      return A
+    case 2:
+      return B
+    case 3:
+      return C
+    default:
+      return A
+  }
+})
+
+</script>
+
+<template>
+<!--  <A v-if="componentType === 1" />
+  <B v-if="componentType === 2" />
+  <C v-if="componentType === 3" />-->
+
+  <component :is="component"/>
+</template>
+
+<style scoped></style>
