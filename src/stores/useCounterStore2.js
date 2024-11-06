48bff6ad71eb3ceb34cd4cfa52a390afb8e3ef9a
Nguyễn Thành Luân <luannguyen@prep.vn>
2024-11-06 09:35:52 +0700

    demo store

--- src/stores/useCounterStore2.js
+++ src/stores/useCounterStore2.js
@@ -1,12 +1,17 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore2 = defineStore('useCounterStore2',
-  () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+export const useCounterStore2 = defineStore('useCounterStore2', () => {
+  const count = () => {
+    return ref({})
   }
-  return { count, doubleCount, increment }
+
+  const abc = {
+    doubleCount: computed(() => count.value * 2),
+    increment: () => {
+      count.value++
+    },
+  }
+
+  return { count, ...abc }
 })
--- src/views/extractData.js
+++ src/views/extractData.js
@@ -2,7 +2,10 @@ import { computed, ref } from 'vue'
 
 const counter = ref(0)
 
+
 const doubleCounter = computed(() => counter.value * 2)
+
+
 
 export {
   counter,
